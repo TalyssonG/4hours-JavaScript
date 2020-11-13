012670c6136921dff13822181e8024bf4bba6411
TalyssonG <lopestalysson99@gmail.com>
2020-11-13 18:36:07 -0300

    Objetos

---
+++ Objetos/scripts05.js
@@ -0,0 +1,22 @@
+let nome = 'rafael'; // string literal
+let idade = 25; //number literal
+let estaAprovado = true; //boolean
+let sobrenome = undefined; // undefined
+let corSelecionado = null; //Redefinir um valor
+
+let cpf = 31062862315;
+let nomeDoPai = 'renato'; 
+let peso = 70;
+
+let pessoa = {
+    nome: 'Talysson',
+    idade: 25,
+    estaAprovado: true,
+    sobrenome: 'lopes',
+    cpf: '31062862315',
+    nomeDoPai: 'renato',
+    peso: 70
+
+};
+
+console.log(pessoa)
---
+++ Tipagem dinâmica/scripts04.js
@@ -0,0 +1,5 @@
+let nome = 'Talysson'; 
+let idade = 25; 
+let estaAprovado = true;
+let sobrenome = undefined;
+
