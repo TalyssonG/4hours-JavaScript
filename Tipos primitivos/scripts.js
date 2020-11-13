27564d34d602016d8a5876d39f85b5225a616c1d
TalyssonG <lopestalysson99@gmail.com>
2020-11-13 18:09:41 -0300

    Tipos primitivos

---
+++ Tipos primitivos/scripts.js
@@ -0,0 +1,7 @@
+let nome = 'rafael'; // string literal
+let idade = 25; //number literal
+let estaAprovado = true; //boolean
+let sobrenome = undefined; // undefined
+let corSelecionado = null; //Redefinir um valor
+
+
