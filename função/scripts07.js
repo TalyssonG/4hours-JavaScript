882052a0ba9d833ef3deea5e094e85369c2f1a3f
TalyssonG <lopestalysson99@gmail.com>
2020-11-15 00:52:20 -0300

    Funções

---
+++ função/scripts07.js
@@ -0,0 +1,33 @@
+// Função
+// Verbo + Substantivo
+/*
+let corSite = "azul";
+function resetaCor(cor,tonalidade){
+    corSite= cor +' '+tonalidade;
+};
+
+console.log(corSite);
+resetaCor("verde","claro");
+console.log(corSite)
+*/
+
+//Primeira prática
+/*
+let saborPizza = "Calabresa";
+function novoSabor(sabor, recheada){
+  saborPizza= sabor + recheada;
+};
+
+console.log(saborPizza);
+novoSabor("Frango","Catupiry");
+console.log(saborPizza);
+*/
+
+let notaAluno = 10;
+function novaNota(B1,B2){
+notaAluno = B1 + B2;
+}
+
+console.log(notaAluno);
+novaNota(8,5);
+console.log(notaAluno)
