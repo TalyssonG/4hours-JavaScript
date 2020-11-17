335033657e8668b7fec160b39dd9b73016e49207
TalyssonG <lopestalysson99@gmail.com>
2020-11-17 19:51:46 -0300

    Tipos de funções

---
+++ Tipos de funções/scripts07.js
@@ -0,0 +1,21 @@
+// Realiza uma tarefa, e não devolve nada
+function dizerNome(){
+    console.log('Talysson');
+
+}
+
+dizerNome();
+
+//Faz um cálculo ou operação e retorna algo
+function MultiplicarPorDois(valor){
+    return valor * 2;
+
+}
+
+
+
+//console.log(MultiplicarPorDois(5))
+
+let resultado = MultiplicarPorDois(5)
+
+console.log(resultado);
