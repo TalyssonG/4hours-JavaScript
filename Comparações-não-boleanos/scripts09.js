5637a0fac4a3a0c7fd32acef012f966e76fa1c1b
TalyssonG <lopestalysson99@gmail.com>
2020-11-18 18:36:45 -0300

    Comparações não boleanas

---
+++ Comparações-não-boleanos/scripts09.js
@@ -0,0 +1,16 @@
+// Falsy 
+
+// undefined
+// null
+// 0
+// false
+// ''
+// NaN - not a number
+
+//Truthy
+
+let corPersonalizada = '';
+let corPadrao = 'blue';
+let corPerfil = corPersonalizada || corPadrao;
+
+console.log(corPerfil);
