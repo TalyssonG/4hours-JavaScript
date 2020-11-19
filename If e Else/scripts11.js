d7ebca0eb9971fa787a218182b4ed58986663c8b
TalyssonG <lopestalysson99@gmail.com>
2020-11-19 14:36:33 -0300

    IF..ELSE

---
+++ If e Else/scripts11.js
@@ -0,0 +1,18 @@
+// IF..Else
+
+// Se a hora estiver entre 06:00 até 12:00 : Bom dia!
+// Se estiver entre 12:00 até 18:00 : Boa tarde!
+
+let hora = 19;
+
+if(hora >= 6 && hora <= 12){
+console.log('São',hora,'Horas, Bom dia!');
+}
+
+else if(hora >=12 && hora <=18){
+console.log('São',hora,' Horas Boa tarde');
+}
+
+else{
+    console.log('São',hora,'Horas Boa noite')
+}
