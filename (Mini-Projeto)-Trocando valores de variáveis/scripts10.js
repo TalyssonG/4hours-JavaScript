f35ba7e7d30e857f772f47da9d1ad4d87e469388
TalyssonG <lopestalysson99@gmail.com>
2020-11-19 14:06:27 -0300

    Trocando valores

---
+++ (Mini-Projeto)-Trocando valores de variáveis/scripts10.js
@@ -0,0 +1,25 @@
+/*
+let a = 'vermelho';
+let b = 'azul'
+
+let c = a;
+a = b;
+b = c;
+
+console.log(a);
+console.log(b);
+*/
+
+// pratica 02
+let pizza = 'Calabresa'
+let pizza2 = 'Frango'
+
+let pizza3 = pizza   
+pizza = pizza2
+pizza2 = pizza3        
+
+
+console.log(pizza)
+console.log(pizza2)
+
+//Frango e calabresa
