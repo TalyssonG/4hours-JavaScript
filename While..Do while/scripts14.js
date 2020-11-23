60aed38f30661484670b3be7dd7969f0d9b7d791
TalyssonG <lopestalysson99@gmail.com>
2020-11-23 20:40:41 -0300

    While..Do while

---
+++ While..Do while/scripts14.js
@@ -0,0 +1,20 @@
+//: while
+
+/*
+let i = 10;
+
+while (i >= 1){
+    if(i % 2 !== 1){
+        console.log('Números pares',i);
+    }
+    i--;
+} */
+
+
+//Do..While
+
+let i = 1;
+do {
+     console.log('digitando!',i);
+     i++; 
+} while(i < 10)
