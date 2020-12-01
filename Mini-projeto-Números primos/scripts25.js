1d12bac555415b690734b479664a0c99ab8c1823
TalyssonG <lopestalysson99@gmail.com>
2020-12-01 17:53:01 -0300

    Mini-projeto-Numeros-primos

---
+++ Mini-projeto-Números primos/scripts25.js
@@ -0,0 +1,30 @@
+//Criar função para mostrar ps números primos
+
+//primos
+//compostos
+
+//Ex: 10
+//10 (divisível por 1 ou por ele mesmo)
+
+//10 - 1,2,4,6,10
+
+//exibirNumerosPrimos(20);
+//function exibirNumerosPrimos(limite){
+
+/*
+    for(let i = 2; i <= limite; i++){
+        let ehPrimo = true;
+
+        for(let divisor = 2; divisor < i; divisor++){
+          if(i % divisor === 0){
+            ehPrimo = false;
+            break;
+          }
+        }
+
+    
+    if(ehPrimo)
+    console.log(i);
+}
+}
+*/
