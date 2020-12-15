9e227da9fc4961ba5b50b4d74cdfd06219db4762
TalyssonG <lopestalysson99@gmail.com>
2020-12-15 14:16:29 -0300

    Template literal

---
+++ Template-Literal/script31.js
@@ -0,0 +1,14 @@
+// Aula 39 - Template Literal
+//object {}
+//boolean true,false
+//string '',""
+//Template ``
+
+const outra = `segunda  msg`
+console.log(outra)
+
+const nome = 'Talysson'
+
+const email = 
+`Olá ${nome}`
+console.log(email)
