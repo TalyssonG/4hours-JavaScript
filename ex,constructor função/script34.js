e64e384fb8728fe5917815ac4fb56dfe2f3e91c4
TalyssonG <lopestalysson99@gmail.com>
2020-12-22 13:59:42 -0300

    Constructor Function- ex

---
+++ ex,constructor função/script34.js
@@ -0,0 +1,18 @@
+// Construtor Function {Exercício}
+
+// Criar um objeto Postagem
+// Titulo, mensagem, autor, visualizações, comentarios[], es
+
+function Postagem (titulo,mensagem, autor){
+    this.titulo = titulo,
+    this.mensagem = mensagem,
+    this.autor = autor,
+    this.visualizacoes = 0,
+    this.comentarios = [
+    
+    ],
+    this.estaAovivo = false
+}
+
+let postagem = new Postagem('JavaScript', 'Olá' ,'Talysson', 200);
+console.log(postagem);
