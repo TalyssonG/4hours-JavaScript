8937895a67a634cc96b68bd82f41e3c9b55e381e
TalyssonG <lopestalysson99@gmail.com>
2020-12-27 22:45:53 -0300

    ForEath

---
+++ Foreach/index.js
@@ -0,0 +1,21 @@
+const numeros = [1,2,3,4,5]
+
+//for (numero of numeros) 
+//    console.log(numero)
+
+//Foreach
+numeros.forEach((numero,indice) =>
+    console.log(numero,indice)
+) 
+
+//exercício
+
+const nomes = ['Talysson','Petrysson','Perera']
+      //for(nome of nomes)
+      //console.log(nome)
+nomes.forEach((nome,indice) =>
+    console.log(nome,indice)
+)
+
+
+
