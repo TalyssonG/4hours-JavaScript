79c90afa2ef585716a5afbac7592fabf895e50e2
TalyssonG <lopestalysson99@gmail.com>
2020-12-28 14:07:27 -0300

    combinando arrays

---
+++ Combinando arrays/index.js
@@ -0,0 +1,26 @@
+//Combinando arrays
+
+const numeros = [1,2,3,4,5]
+
+const combinado = numeros.join('.')
+console.log(combinado)
+
+
+//const frase = 'Talysson papi'
+//const resultado = frase.split(' ')
+
+//console.log(resultado.join('-'))
+
+
+//Exercício
+
+const nomes = ['Talysson', 'papi', 'perera']
+
+const combinar = nomes.join('.')
+console.log(combinar)
+
+const url = "olá esse é meu site"
+const resultado = url.split(' , ')
+console.log(resultado)
+
+console.log(resultado.join('-'))
